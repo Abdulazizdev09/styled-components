f0c0d4df66bf01e2fd66d3be8d7217fdd9fff909
Yusupov_dev <yusupovabdulaziz222@gmail.com>
2025-09-04 15:29:35 +0500

    simple button created using styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from "styled-components"
+
+const StyledButton = styled.button`
+  background-color: #4CAF50;  /* Green */
+  color: white;              /* Text color */
+  padding: 10px 20px;        /* Space inside */
+  border: none;              /* No border */
+  border-radius: 8px;        /* Rounded corners */
+  cursor: pointer;           /* Hand icon */
+  font-size: 16px;           /* Text size */
+  transition: background 0.3s ease;
+
+  &:hover {
+    background-color: #45a049; /* Darker green on hover */
+  }
+
+  &:active {
+    background-color: #3e8e41; /* Even darker when clicked */
+  }
+`
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      {/* <button>Button</button> */}
+      <StyledButton>Styled button </StyledButton>
+    </div>
   )
 }
 
